7c25834668f60c9d581263bb12e08eccf24833bd
LinArcX <linarcx@gmail.com>
2017-12-31 11:00:29 +0330

    nothing

--- modules/launcher/presenter/launcher.cpp
+++ modules/launcher/presenter/launcher.cpp
@@ -18,6 +18,8 @@ Launcher::Launcher(QObject* parent)
 {
     //history 50 | awk '{h[$2]++}END{for(i in h){print h[i],i|"sort -rn|head -20"}}'
 
+    //last reboot
+
     //iwgetid -r; cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address;
 }
 
