4d75b4782d60bc96aeb5ef152ca2bb02681cdcc4
linarcx <linarcx@riseup.net>
2019-08-11 00:15:16 +0430

    delete ncurses lib and re-write .gitignore file

--- modules/core/main.cpp
+++ modules/core/main.cpp
@@ -1,8 +1,6 @@
 #include "modules/core/dispatcher/dispatcher.h"
 #include <QApplication>
 
-#include <ncurses.h>
-
 int main(int argc, char* argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
